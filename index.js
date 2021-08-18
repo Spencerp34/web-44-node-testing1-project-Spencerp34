43a3372b45126adc0ba248d93c061e9800dd2054
Spencerp34 <spencerp34@yahoo.com>
2021-08-18 16:51:27 -0600

    trying Gabes way

--- index.js
+++ index.js
@@ -147,26 +147,26 @@ class Car {
    */
   drive(distance) {
     
-    this.tank = this.tankSize - (distance/this.mpg)
-    if(this.tank < 0){
-      this.tank = 0
-    }
-    this.odometer += distance
-    if(distance > this.maxDist){
-      this.tank = 0
-    }
-    return this.odometer
+    // this.tank = this.tankSize - (distance/this.mpg)
+    // if(this.tank < 0){
+    //   this.tank = 0
+    // }
+    // this.odometer += distance
+    // if(distance > this.maxDist){
+    //   this.tank = 0
+    // }
+    // return this.odometer
 
 
     //GABES WAY
-    // if(distance <= this.maxDist){
-    //   this.odometer += distance
-    //   this.tank -= (distance/this.mpg)
-    //   return this.odometer
-    // }
-    // this.odometer += this.maxDist
-    // this.tank = 0
-    // return this.odometer
+    if(distance <= this.maxDist){
+      this.odometer += distance
+      this.tank -= (distance/this.mpg)
+      return this.odometer
+    }
+    this.odometer += this.maxDist
+    this.tank = 0
+    return this.odometer
 
   }
 
